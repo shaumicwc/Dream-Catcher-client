597891069873e37b91c03d73196a9ea4ca0a1d00
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-04-10 03:32:33 +0600

    header added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,18 +4,20 @@ import './Header.css'
 
 const Header = () => {
     const location = useLocation();
+    const jobDetails = location.pathname === '/jobDetails'
     const isStatisticsActive = location.pathname === '/statistics';
     const isAppliedJobsActive = location.pathname === '/appliedJobs';
     const isBlogActive = location.pathname === '/blog';
 
     return (
-        <nav className={isStatisticsActive === true ? 'statistics' : isAppliedJobsActive === true ? 'appliedJobs' : isBlogActive === true ? 'blog' : 'nav'}>
-            {
+       <div className='main'>
+         <nav className={isStatisticsActive === true ? 'statistics' : isAppliedJobsActive === true ? 'appliedJobs' : isBlogActive === true ? 'blog' : 'nav'}>
+             {
                 isAppliedJobsActive || isBlogActive || isStatisticsActive === true ? <img className='header-img-1' src="./src/assets/Images/All Images/Vector.png" alt="" /> : ''
             }
-            <div className='navbar'>
+            <div className={isAppliedJobsActive || isBlogActive || isStatisticsActive === true ? 'navbarS' : 'navbar'}>
                 <div className='navbar-link'>
-                    <h1>Dream Catcher</h1>
+                    <Link className='home-link' to='/'><h1>Dream Catcher</h1></Link>
                     <div>
                         <Link to='/statistics'>Statistics</Link>
                         <Link to='/appliedJobs'>Applied Jobs</Link>
@@ -23,13 +25,25 @@ const Header = () => {
                     </div>
                     <button>Start Applying</button>
                 </div>
-                <div></div>
-            
+                <div className='nav-tag'>
+                    {
+                        isStatisticsActive === true ? 'Statistics' : isAppliedJobsActive === true ? 'Applied Jobs' : isBlogActive === true ? 'Blog' : jobDetails === true ? 'Job Details' : '' 
+                    }
+                </div>
             </div>
             {
                 isAppliedJobsActive || isBlogActive || isStatisticsActive === true ? <img className='header-img-2' src="./src/assets/Images/All Images/Vector-1.png" alt="" /> : ''
             }
         </nav>
+        <div className='header'>
+            <div className='header-text'>
+                <h1>One Step Closer To Your <span>Dream Job</span></h1>
+                <p>Explore thousands of job opportunities with all the information you need. Its your future. Come find it. Manage all your job application from start to finish.</p>
+                <button>Get Started</button>           
+            </div>
+            <img src="/src/assets/images/All Images/P3OLGJ1 copy 1.png" alt="" />
+        </div>
+       </div>
     );
 };
 

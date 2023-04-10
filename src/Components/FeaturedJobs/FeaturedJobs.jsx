be9eca838e4269b7bdbee5ba69d26d4878fc11ee
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-04-10 18:16:33 +0600

    see all jobs btn functionality done

--- src/Components/FeaturedJobs/FeaturedJobs.jsx
+++ src/Components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,34 +1,46 @@
 import React, { useEffect, useState } from 'react';
-import './FeaturedJobs.css'
+import './FeaturedJobs.css';
 import FeaturedJobsCards from '../FeaturedJobsCards/FeaturedJobsCards';
-import {  addToDb, getJobCart } from '../../utilities/fakedb';
+import { addToDb } from '../../utilities/fakedb';
 
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
+    const [allJobs, setAllJobs] = useState([])
+
     useEffect(() => {
         fetch('Featured.json')
-        .then(res => res.json())
-        .then(data => setJobs(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setJobs(data.slice(0, 4)))
+    }, [])
 
     const handleViewDetails = (job) => {
-        addToDb(job.id)
+        addToDb(job.id);
     }
-    
+
+    const handleSeeAllJobs = () => {
+        fetch('Featured.json')
+            .then(res => res.json())
+            .then(data => setAllJobs(data));
+    }
+    console.log(allJobs)
     return (
         <div className='featured'>
             <h1>Featured Jobs</h1>
             <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             <div className='jobs-card'>
-                {
-                    jobs.map(job => <FeaturedJobsCards
-                        handleViewDetails={handleViewDetails}
+                {allJobs.length > 0 ? allJobs.map(job => <FeaturedJobsCards
+                    handleViewDetails={handleViewDetails}
                     key={job.id}
                     job={job}
-                    ></FeaturedJobsCards>)
+                ></FeaturedJobsCards>) :
+                    jobs.map(job => <FeaturedJobsCards
+                        handleViewDetails={handleViewDetails}
+                        key={job.id}
+                        job={job}
+                    />)
                 }
             </div>
-            
+            <button onClick={handleSeeAllJobs}>See all jobs</button>
         </div>
     );
 };

83db23cc30646a1060fe26753e21037a4da70d9c
shaumicwc <shaumic_chowdhury@hotmail.com>
2023-04-10 18:10:53 +0600

    applied sorted by remote or onsite button

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import AppliedJobsCard from '../AppliedJobsCard/AppliedJobsCard';
-import './AppliedJobs.css'
+import './AppliedJobs.css';
 
 const AppliedJobs = () => {
-    const data = useLoaderData();
-console.log(data)
-    return (
-        <div className='applied-jobs'>
-            <div className='sort-btn'>
-                <button>Sort by Remote</button>
-                <button>Sort by Onsite</button>
-            </div>
-            {
-                data.map(singleData => <AppliedJobsCard
-                singleData={singleData}
-                key={singleData.id}
-                ></AppliedJobsCard>)
-            }
-        </div>
-    );
+  const data = useLoaderData();
+  const [remoteOrOnsiteJobs, setRemoteOrOnsiteJobs] = useState([]);
+
+  const handleRemoteSort = (remote) => {
+    const jobs = data.filter((job) => job.remoteOrOnsite === remote);
+    setRemoteOrOnsiteJobs(jobs);
+  };
+
+  return (
+    <div className='applied-jobs'>
+      <div className='sort-btn'>
+        <button onClick={() => handleRemoteSort('Remote')}>Sort by Remote</button>
+        <button onClick={() => handleRemoteSort('Onsite')}>Sort by Onsite</button>
+      </div>
+      {remoteOrOnsiteJobs.length > 0 ? (
+        remoteOrOnsiteJobs.map((job) => <AppliedJobsCard singleData={job} key={job.id} />)
+      ) : (
+        data.map((job) => <AppliedJobsCard singleData={job} key={job.id} />)
+      )}
+    </div>
+  );
 };
 
 export default AppliedJobs;
